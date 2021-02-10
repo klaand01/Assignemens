f38c3bac3c2f050b5722ca8a89bd084032d7f460
Klara Andersson <kmda23@hotmail.com>
2021-02-10 13:57:16 +0100

    Initial commit

---
+++ clientmain.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/* You will to add includes here */
+
+
+// Enable if you want debugging to be printed, see examble below.
+// Alternative, pass 
+#define DEBUG
+
+
+// Included to get the support library
+#include <calcLib.h>
+
+int main(int argc, char *argv[])
+{
+  /*
+    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
+     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
+  */
+ 
+  if (argc != 2)
+  {
+    printf("Usage: %s hostname:port (%d)\n", argv[0], argc);
+  }
+  
+  char delim[]=":";
+  char *Desthost=strtok(argv[1],delim);
+  char *Destport=strtok(NULL,delim);
+  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
+  // *Dstport points to whatever string came after the delimiter. 
+
+  /* Do magic */
+  int port=atoi(Destport);
+#ifdef DEBUG 
+  printf("Host %s, and port %d.\n",Desthost,port);
+#endif
+
+  
+}
