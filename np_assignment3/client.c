1188a7fb200b74341c2cf4436eda136719f95f8c
Klara Andersson <kmda23@hotmail.com>
2021-02-27 15:53:30 +0100

    Fixing nickname

--- np_assignment3/client.c
+++ np_assignment3/client.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <regex.h>
 
 #define MAXDATA 255
 
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
   
   if (argc != 3)
   {
-    printf("Usage: %s hostname port nickname (%d)\n", argv[0], argc);
+    printf("Usage: %s hostname:port nickname (%d)\n", argv[0], argc);
     exit(1);
   }
 
@@ -23,9 +24,8 @@ int main(int argc, char *argv[])
   char *Desthost=strtok(argv[1],delim);
   char *Destport=strtok(NULL,delim);
   int port=atoi(Destport);
-
-  printf("2 argument: %s \n", argv[2]);
   printf("Connected to %s:%s \n", Desthost, Destport);
+  char *name = argv[2];
 
   struct addrinfo addrs, *ptr;
   memset(&addrs, 0, sizeof(addrs));
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
   }
 
   while (1)
-  {
+  {    
     memset(&buf, 0, sizeof(buf));
 
     numbrBytes = recv(clientSocket, buf, MAXDATA, 0);
@@ -71,6 +71,7 @@ int main(int argc, char *argv[])
       perror("Wrong with message \n");
       exit(1);
     }
+    printf("Server: '%s' \n", buf);
 
     if (strcmp(buf, "HELLO 1\n") == 0)
     {
@@ -81,6 +82,16 @@ int main(int argc, char *argv[])
       perror("Protocol not supported, closing down \n");
       exit(1);
     }
+
+    numbrBytes = send(clientSocket, name, strlen(name), 0);
+    printf("Name: %s \n", name);
+    if (numbrBytes == -1)
+    {
+      perror("Send not gone through \n");
+      exit(1);
+    }
+    printf("Sent name \n");
+
   }
 
   close(clientSocket);
