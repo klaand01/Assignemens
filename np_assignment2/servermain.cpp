972f15c40e3ff0281571e15d91e14a580e6fd8bf
Klara Andersson <kmda23@hotmail.com>
2021-03-03 16:10:58 +0100

    Working on server

--- np_assignment2/servermain.cpp
+++ np_assignment2/servermain.cpp
@@ -38,12 +38,6 @@ void checkJobbList(int signum)
   client.clientID = 0;
 
   printf("Let me be, I want to sleep.\n");
-
-  if(loopCount>20)
-  {
-    printf("I had enough.\n");
-    terminate=1;
-  }
   
   return;
 }
@@ -68,6 +62,19 @@ int main(int argc, char *argv[])
 
   int iNumb1, iNumb2, iRes, iDiff;
   double dNumb1, dNumb2, dRes, dDiff;
+
+  /* 
+     Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
+  */
+  struct itimerval alarmTime;
+  alarmTime.it_interval.tv_sec=10;
+  alarmTime.it_interval.tv_usec=10;
+  alarmTime.it_value.tv_sec=10;
+  alarmTime.it_value.tv_usec=10;
+
+  /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
+  signal(SIGALRM, checkJobbList);
+  setitimer(ITIMER_REAL,&alarmTime,NULL); // Start/register the alarm.
 
   struct addrinfo addrs, *ptr;
   memset(&addrs, 0, sizeof(addrs));
@@ -103,145 +110,143 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  /* 
-     Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
-  */
-  struct itimerval alarmTime;
-  alarmTime.it_interval.tv_sec=10;
-  alarmTime.it_interval.tv_usec=10;
-  alarmTime.it_value.tv_sec=10;
-  alarmTime.it_value.tv_usec=10;
+  initCalcLib();
+  calcMessage *cMessage = new calcMessage;
+  calcProtocol *cProtocol = new calcProtocol;
+  calcProtocol *tempProtocol = new calcProtocol;
 
-  /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
-  signal(SIGALRM, checkJobbList);
-  setitimer(ITIMER_REAL,&alarmTime,NULL); // Start/register the alarm.
-
-  struct calcMessage cMessage;
-  struct calcProtocol cProtocol;
-
-  while(terminate==0)
+  while(1)
   {
     printf("This is the main loop, %d time.\n",loopCount);
-    
-    sentBytes = recvfrom(serverSocket, &cMessage, sizeof(cMessage), 0, ptr->ai_addr, &ptr->ai_addrlen);
+
+    sentBytes = recvfrom(serverSocket, &tempProtocol, sizeof(tempProtocol), 0, ptr->ai_addr, &ptr->ai_addrlen);
     if (sentBytes == -1)
     {
       perror("Message not received \n");
     }
+    if (sentBytes == 0)
+    {
+      perror("Nothing recieved \n");
+      close(serverSocket);
+    }
     else
     {
-      printf("CalcMessage received \n");
+      printf("Message received \n");
     }
 
+    //Check client
     in_addr ipAddrs = ((sockaddr_in*)ptr)->sin_addr;
     client.ipAddress = inet_ntoa(ipAddrs);
-    client.clientID = ntohl(cProtocol.id);
+    client.clientID = ntohl(cProtocol->id);
 
-    cMessage.type = ntohs(cMessage.type);
-    cMessage.message = ntohl(cMessage.message);
-    cMessage.protocol = ntohs(cMessage.protocol);
-    cMessage.major_version = ntohs(cMessage.major_version);
-
-    if (cMessage.type != 22 || cMessage.message != 0 || 
-    cMessage.protocol != 17 || cMessage.major_version != 1)
-    {
-      cMessage.message = htons(2);
-      sentBytes = sendto(serverSocket, &cMessage, sizeof(cMessage), 0, ptr->ai_addr, ptr->ai_addrlen);
-      printf("Wrong message received \n");
-    }
-
-    initCalcLib();
-    char *oper = randomType();
-    iNumb1 = randomInt();
-    iNumb2 = randomInt();
-    dNumb1 = randomFloat();
-    dNumb2 = randomFloat();
-
-    cProtocol.inValue1 = htonl(iNumb1);
-    cProtocol.inValue2 = htonl(iNumb2);
-    cProtocol.flValue1 = dNumb1;
-    cProtocol.flValue2 = dNumb2;
-
-    if (strcmp(oper, "add") == 0)
-    {
-      cProtocol.arith = htonl(1);
-      iRes = iNumb1 + iNumb2;
-    }
-
-    if (strcmp(oper, "sub") == 0)
-    {
-      cProtocol.arith = htonl(2);
-      iRes = iNumb1 - iNumb2;
-    }
-
-    if (strcmp(oper, "mul") == 0)
-    {
-      cProtocol.arith = htonl(3);
-      iRes = iNumb1 * iNumb2;
-    }
-
-    if (strcmp(oper, "div") == 0)
-    {
-      cProtocol.arith = htonl(4);
-      iRes = iNumb1 / iNumb2;
-    }
-
-    if (strcmp(oper, "fadd") == 0)
-    {
-      cProtocol.arith = htonl(5);
-      dRes = dNumb1 + dNumb2;
-    }
-
-    if (strcmp(oper, "fsub") == 0)
-    {
-      cProtocol.arith = htonl(6);
-      dRes = dNumb1 - dNumb2;
-    }
-
-    if (strcmp(oper, "fmul") == 0)
-    {
-      cProtocol.arith = htonl(7);
-      dRes = dNumb1 * dNumb2;
-    }
-
-    if (strcmp(oper, "fdiv") == 0)
-    {
-      cProtocol.arith = htonl(8);
-      dRes = dNumb1 / dNumb2;
-    }
-
-    sentBytes = sendto(serverSocket, &cProtocol, sizeof(cProtocol), 0, ptr->ai_addr, ptr->ai_addrlen);
-    if (sentBytes == -1)
-    {
-      perror("Calcprotocol not sent \n");
-    }
-    else
-    {
-      printf("Calcprotocol sent \n");
-    }
-
-    sentBytes = recvfrom(serverSocket, &cProtocol, sizeof(cProtocol), 0, ptr->ai_addr, &ptr->ai_addrlen);
-    if (sentBytes == -1)
-    {
-      perror("Calcprotocol not received \n");
-      exit(1);
-    }
-    else if (client.ipAddress != inet_ntoa(ipAddrs) || client.clientID != ntohl(cProtocol.id))
+    if (client.ipAddress != inet_ntoa(ipAddrs) || client.clientID != ntohl(cProtocol->id))
     {
       printf("Client is dead to me now \n");
-      cMessage.message = ntohl(2);
+      cMessage->message = ntohl(2);
       sentBytes = sendto(serverSocket, &cMessage, sizeof(cMessage), 0, ptr->ai_addr, ptr->ai_addrlen);
     }
-    else
+
+    if (sentBytes == sizeof(cMessage))
+    {
+      printf("Calcmessage recieved \n");
+      cMessage = (calcMessage *) tempProtocol;
+      
+      cMessage->type = ntohs(cMessage->type);
+      cMessage->message = ntohl(cMessage->message);
+      cMessage->protocol = ntohs(cMessage->protocol);
+      cMessage->major_version = ntohs(cMessage->major_version);
+
+      printf("Type: %d Message: %d Protocol: %d Majorversion %d\n", cMessage->type, cMessage->message,
+        cMessage->protocol, cMessage->major_version);
+
+      if (cMessage->type != 22 || cMessage->message != 0 || 
+      cMessage->protocol != 17 || cMessage->major_version != 1)
+      {
+        cMessage->message = htons(2);
+        sentBytes = sendto(serverSocket, &cMessage, sizeof(cMessage), 0, ptr->ai_addr, ptr->ai_addrlen);
+        printf("Wrong message received \n");
+      }
+
+      //Räkneoperationer
+      char *oper = randomType();
+      iNumb1 = randomInt();
+      iNumb2 = randomInt();
+      dNumb1 = randomFloat();
+      dNumb2 = randomFloat();
+
+      cProtocol->inValue1 = htonl(iNumb1);
+      cProtocol->inValue2 = htonl(iNumb2);
+      cProtocol->flValue1 = dNumb1;
+      cProtocol->flValue2 = dNumb2;
+
+      if (strcmp(oper, "add") == 0)
+      {
+        cProtocol->arith = htonl(1);
+        iRes = iNumb1 + iNumb2;
+      }
+
+      if (strcmp(oper, "sub") == 0)
+      {
+        cProtocol->arith = htonl(2);
+        iRes = iNumb1 - iNumb2;
+      }
+
+      if (strcmp(oper, "mul") == 0)
+      {
+        cProtocol->arith = htonl(3);
+        iRes = iNumb1 * iNumb2;
+      }
+
+      if (strcmp(oper, "div") == 0)
+      {
+        cProtocol->arith = htonl(4);
+        iRes = iNumb1 / iNumb2;
+      }
+
+      if (strcmp(oper, "fadd") == 0)
+      {
+        cProtocol->arith = htonl(5);
+        dRes = dNumb1 + dNumb2;
+      }
+
+      if (strcmp(oper, "fsub") == 0)
+      {
+        cProtocol->arith = htonl(6);
+        dRes = dNumb1 - dNumb2;
+      }
+
+      if (strcmp(oper, "fmul") == 0)
+      {
+        cProtocol->arith = htonl(7);
+        dRes = dNumb1 * dNumb2;
+      }
+
+      if (strcmp(oper, "fdiv") == 0)
+      {
+        cProtocol->arith = htonl(8);
+        dRes = dNumb1 / dNumb2;
+      }
+
+      sentBytes = sendto(serverSocket, &cProtocol, sizeof(cProtocol), 0, ptr->ai_addr, ptr->ai_addrlen);
+      if (sentBytes == -1)
+      {
+        perror("Calcprotocol not sent \n");
+      }
+      else
+      {
+        printf("Calcprotocol sent \n");
+      }
+    }
+    else if (sentBytes == sizeof(cProtocol))
     {
       printf("Calcprotocol received \n");
 
-      iDiff = iRes - ntohs(cProtocol.inResult);
-      dDiff = dRes - cProtocol.flResult;
+      iDiff = iRes - ntohs(cProtocol->inResult);
+      dDiff = dRes - cProtocol->flResult;
 
       if (iDiff < 0.0001 || dDiff < 0.0001)
       {
-        cMessage.message = htonl(1);
+        cMessage->message = htonl(1);
         sentBytes = sendto(serverSocket, &cMessage, sizeof(cMessage), 0, ptr->ai_addr, ptr->ai_addrlen);
         if (sentBytes == -1)
         {
